f2cbc5f7d7f2bf5e292b5c8d49192e904391df4d
samrg472 <samrg472@gmail.com>
2015-03-09 18:03:04 -0400

    Fix event bus warnings

--- sdk/core/src/main/java/org/dsa/iot/core/event/EventBusFactory.java
+++ sdk/core/src/main/java/org/dsa/iot/core/event/EventBusFactory.java
@@ -1,8 +1,11 @@
 package org.dsa.iot.core.event;
 
+import lombok.val;
 import net.engio.mbassy.bus.MBassador;
+import net.engio.mbassy.bus.common.Properties;
 import net.engio.mbassy.bus.config.BusConfiguration;
 import net.engio.mbassy.bus.config.Feature;
+import net.engio.mbassy.bus.error.IPublicationErrorHandler;
 
 /**
  * @author Samuel Grenier
@@ -10,10 +13,14 @@ import net.engio.mbassy.bus.config.Feature;
 public class EventBusFactory {
     
     public static MBassador<Event> create() {
-        BusConfiguration config = new BusConfiguration();
+        val config = new BusConfiguration();
         config.addFeature(Feature.SyncPubSub.Default());
         config.addFeature(Feature.AsynchronousHandlerInvocation.Default());
         config.addFeature(Feature.AsynchronousMessageDispatch.Default());
+
+        val logger = new IPublicationErrorHandler.ConsoleLogger();
+        config.setProperty(Properties.Handler.PublicationError, logger);
+
         return new MBassador<>(config);
     }
 }
