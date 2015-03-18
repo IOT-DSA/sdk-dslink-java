ce91f38e18a09b2baf4276cd5eb9cdd715173020
samrg472 <samrg472@gmail.com>
2015-03-18 17:09:45 -0400

    Add javadoc to actions package

---
+++ sdk/dslink/src/main/java/org/dsa/iot/dslink/node/actions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * API for managing actions within a node. Such as parameters, columns,
+ * invocations, permissions, etc...
+ * @author Samuel Grenier
+ */
+package org.dsa.iot.dslink.node.actions;
