cc5531e2dd9462af9153396d63149ed1dc50d036
samrg472 <samrg472@gmail.com>
2015-03-27 18:08:32 -0400

    Display location

--- ci/script/build.sh
+++ ci/script/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Perform a raw build
-./gradlew javadoc assemble test
+./gradlew distZip test
--- ci/script/deploy.sh
+++ ci/script/deploy.sh
@@ -4,6 +4,7 @@
 if [[ $TRAVIS_BRANCH != 'master' || $TRAVIS_PULL_REQUEST == true ]]; then
     echo 'Ignoring artifact upload'
 else
+    echo "$LOC"
     ./gradlew "-Psigning.secretKeyRingFile=$PWD/$LOC" \
                 "-Psigning.keyId=$ID" \
                 "-Psigning.password=$KEY_PASS" \
