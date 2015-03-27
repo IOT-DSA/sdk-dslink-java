ae8c4d7c0886a0ab0275eaf1529e76efef2f6a97
samrg472 <samrg472@gmail.com>
2015-03-27 18:39:20 -0400

    Check is user and pass are empty

--- ci/script/deploy.sh
+++ ci/script/deploy.sh
@@ -4,6 +4,12 @@
 if [[ $TRAVIS_BRANCH != 'master' || $TRAVIS_PULL_REQUEST == true ]]; then
     echo 'Ignoring artifact upload'
 else
+    if [ -z "$USER" ]; then
+        echo "User is empty"
+    fi
+    if [ -z "$PASS" ]; then
+        echo "Passowrd is empty"
+    fi
     ./gradlew "-Psigning.secretKeyRingFile=$PWD/$LOC" \
                 "-Psigning.keyId=$ID" \
                 "-Psigning.password=$KEY_PASS" \
