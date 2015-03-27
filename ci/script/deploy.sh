4629bffb4fc2d5a9e9846c71fd7fdeda707a5aff
samrg472 <samrg472@gmail.com>
2015-03-27 18:12:43 -0400

    Remove location echo

--- ci/script/deploy.sh
+++ ci/script/deploy.sh
@@ -4,7 +4,6 @@
 if [[ $TRAVIS_BRANCH != 'master' || $TRAVIS_PULL_REQUEST == true ]]; then
     echo 'Ignoring artifact upload'
 else
-    echo "$LOC"
     ./gradlew "-Psigning.secretKeyRingFile=$PWD/$LOC" \
                 "-Psigning.keyId=$ID" \
                 "-Psigning.password=$KEY_PASS" \
