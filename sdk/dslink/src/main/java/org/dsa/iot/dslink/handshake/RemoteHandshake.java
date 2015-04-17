a60f8e5491640ceb56dab67fb40afde2ef6acff0
samrg472 <samrg472@gmail.com>
2015-04-17 18:48:12 -0400

    Enforce a timeout when waiting for the handshake to complete

--- sdk/dslink/src/main/java/org/dsa/iot/dslink/handshake/RemoteHandshake.java
+++ sdk/dslink/src/main/java/org/dsa/iot/dslink/handshake/RemoteHandshake.java
@@ -12,6 +12,7 @@ import org.vertx.java.core.json.JsonObject;
 
 import java.net.HttpURLConnection;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Handshake information retrieved from the server.
@@ -161,7 +162,7 @@ public class RemoteHandshake {
         req.end(lh.toJson().encode());
 
         try {
-            latch.await();
+            latch.await(5, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             h.setFailure(e);
         }
