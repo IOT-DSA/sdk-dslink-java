c00257fd0f5257cdf256674ca6b3c5e3281b4ac8
samrg472 <samrg472@gmail.com>
2015-07-14 09:48:41 -0700

    Provide overridable root historian node functionality

--- sdk/historian/src/main/java/org/dsa/iot/historian/Historian.java
+++ sdk/historian/src/main/java/org/dsa/iot/historian/Historian.java
@@ -32,27 +32,51 @@ public abstract class Historian extends DSLinkHandler {
     @Override
     public final void onResponderInitialized(DSLink link) {
         Node superRoot = link.getNodeManager().getSuperRoot();
-        {
-            NodeBuilder b = superRoot.createChild("createDb");
-            b.setDisplayName("Create Database");
-            b.setAction(provider.createDbAction(provider.dbPermission()));
-            b.build();
-        }
-
-        Map<String, Node> children = superRoot.getChildren();
-        if (children != null) {
-            for (final Node node : children.values()) {
-                Value v = node.getRoConfig("db");
-                if (v != null && v.getBool()) {
-                    provider.createAndInitDb(node);
-                }
-            }
-        }
+        initialize(superRoot);
     }
 
     @Override
     public final void onRequesterConnected(DSLink link) {
 
+    }
+
+    /**
+     * Initializes the node and all its children as a historian.
+     *
+     * @param node Historian root node.
+     */
+    public void initialize(Node node) {
+        initCreateDb(node);
+        iterateDatabaseChildren(node);
+    }
+
+    /**
+     * Initializes the create database action.
+     *
+     * @param node Node to initialize database creation.
+     */
+    protected void initCreateDb(Node node) {
+        NodeBuilder b = node.createChild("createDb");
+        b.setDisplayName("Create Database");
+        b.setAction(provider.createDbAction(provider.dbPermission()));
+        b.build();
+    }
+
+    /**
+     * Iterates the children of the node and initializes the database.
+     *
+     * @param node Database container node.
+     */
+    protected void iterateDatabaseChildren(Node node) {
+        Map<String, Node> children = node.getChildren();
+        if (children != null) {
+            for (final Node n : children.values()) {
+                Value v = n.getRoConfig("db");
+                if (v != null && v.getBool()) {
+                    provider.createAndInitDb(n);
+                }
+            }
+        }
     }
 
     /**
