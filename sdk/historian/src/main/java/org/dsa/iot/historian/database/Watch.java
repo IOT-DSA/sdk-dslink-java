321f5ebfcfb22e5b92b358b6403ca768ce8067f8
samrg472 <samrg472@gmail.com>
2015-07-14 16:51:04 -0700

    Implement get history

--- sdk/historian/src/main/java/org/dsa/iot/historian/database/Watch.java
+++ sdk/historian/src/main/java/org/dsa/iot/historian/database/Watch.java
@@ -10,6 +10,7 @@ import org.dsa.iot.dslink.node.value.SubscriptionValue;
 import org.dsa.iot.dslink.node.value.Value;
 import org.dsa.iot.dslink.node.value.ValuePair;
 import org.dsa.iot.dslink.node.value.ValueType;
+import org.dsa.iot.historian.stats.GetHistory;
 import org.dsa.iot.historian.utils.QueryData;
 import org.dsa.iot.historian.utils.TimeParser;
 import org.vertx.java.core.Handler;
@@ -75,21 +76,25 @@ public class Watch {
     public void init(Permission perm, Node node) {
         path = node.getName().replaceAll("%2F", "/");
         initData(node);
+        GetHistory.initAction(node, group.getDb());
 
-        NodeBuilder b = node.createChild("unsubscribe");
-        b.setSerializable(false);
-        b.setDisplayName("Unsubscribe");
-        b.setAction(new Action(perm, new Handler<ActionResult>() {
-            @Override
-            public void handle(ActionResult event) {
-                Node node = event.getNode().getParent();
-                node.getParent().removeChild(node);
+        {
+            NodeBuilder b = node.createChild("unsubscribe");
+            b.setSerializable(false);
+            b.setDisplayName("Unsubscribe");
+            b.setAction(new Action(perm, new Handler<ActionResult>() {
+                @Override
+                public void handle(ActionResult event) {
+                    Node node = event.getNode().getParent();
+                    node.getParent().removeChild(node);
 
-                SubscriptionPool pool = group.getDb().getProvider().getPool();
-                pool.unsubscribe(path, Watch.this);
-            }
-        }));
-        b.build();
+                    DatabaseProvider provider = group.getDb().getProvider();
+                    SubscriptionPool pool = provider.getPool();
+                    pool.unsubscribe(path, Watch.this);
+                }
+            }));
+            b.build();
+        }
     }
 
     protected void initData(final Node node) {
---
+++ sdk/historian/src/main/java/org/dsa/iot/historian/stats/GetHistory.java
@@ -0,0 +1,143 @@
+package org.dsa.iot.historian.stats;
+
+import org.dsa.iot.dslink.node.Node;
+import org.dsa.iot.dslink.node.NodeBuilder;
+import org.dsa.iot.dslink.node.Permission;
+import org.dsa.iot.dslink.node.actions.*;
+import org.dsa.iot.dslink.node.actions.table.Row;
+import org.dsa.iot.dslink.node.actions.table.Table;
+import org.dsa.iot.dslink.node.value.Value;
+import org.dsa.iot.dslink.node.value.ValueType;
+import org.dsa.iot.historian.database.Database;
+import org.dsa.iot.historian.utils.QueryData;
+import org.dsa.iot.historian.utils.TimeParser;
+import org.vertx.java.core.Handler;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @author Samuel Grenier
+ */
+public class GetHistory implements Handler<ActionResult> {
+
+    private final Database db;
+    private final String path;
+
+    public GetHistory(String path, Database db) {
+        this.path = path;
+        this.db = db;
+    }
+
+    @Override
+    public void handle(ActionResult event) {
+        long from;
+        long to;
+        {
+            Value v = event.getParameter("Timerange");
+            if (v != null) {
+                String range = event.getParameter("Timerange").getString();
+                String[] split = range.split("/");
+
+                final String sFrom = split[0];
+                final String sTo = split[1];
+
+                from = TimeParser.parse(sFrom);
+                to = TimeParser.parse(sTo);
+            } else {
+                // Assume date is today
+                Calendar c = Calendar.getInstance();
+                Date date = new Date();
+                c.setTime(date);
+                c.set(Calendar.HOUR_OF_DAY, 0);
+                c.set(Calendar.MINUTE, 0);
+                c.set(Calendar.SECOND, 0);
+                c.set(Calendar.MILLISECOND, 0);
+                from = c.getTime().getTime();
+                to = date.getTime();
+            }
+        }
+
+
+        final String sInterval = event.getParameter("Interval").getString();
+        final String sRollup = event.getParameter("Rollup").getString();
+        final Interval interval = Interval.parse(sInterval, sRollup);
+
+        final Table table = event.getTable();
+        db.query(path, from, to, new Handler<QueryData>() {
+            @Override
+            public void handle(QueryData event) {
+                Row row;
+                Value value = event.getValue();
+                long time = event.getTimestamp();
+                if (interval == null) {
+                    row = new Row();
+                    String t = TimeParser.parse(time);
+                    row.addValue(new Value(t));
+                    row.addValue(event.getValue());
+                } else {
+                    row = interval.getRowUpdate(value, time);
+                }
+
+                if (row != null) {
+                    table.addRow(row);
+                }
+            }
+        });
+    }
+
+    public static void initAction(Node node, Database db) {
+        String path = node.getName().replaceAll("%2F", "/");
+        Action a =  new Action(Permission.READ, new GetHistory(path, db));
+        initProfile(a);
+
+        NodeBuilder b = node.createChild("getHistory", "getHistory");
+        b.setAction(a);
+        b.build();
+    }
+
+    private static void initProfile(Action act) {
+        {
+            Parameter param = new Parameter("Timerange", ValueType.STRING);
+            param.setEditorType(EditorType.DATE_RANGE);
+            act.addParameter(param);
+        }
+
+        {
+            Value def = new Value("none");
+            Parameter param = new Parameter("Interval", ValueType.STRING, def);
+            act.addParameter(param);
+        }
+
+        {
+            Set<String> enums = new LinkedHashSet<>();
+            enums.add("none");
+            enums.add("avg");
+            enums.add("min");
+            enums.add("max");
+            enums.add("sum");
+            enums.add("first");
+            enums.add("last");
+            enums.add("count");
+            enums.add("delta");
+            ValueType e = ValueType.makeEnum(enums);
+            Parameter param = new Parameter("Rollup", e);
+            act.addParameter(param);
+        }
+
+        {
+            Parameter param = new Parameter("timestamp", ValueType.TIME);
+            act.addResult(param);
+        }
+
+        {
+            Parameter param = new Parameter("value", ValueType.DYNAMIC);
+            act.addResult(param);
+        }
+
+        act.setResultType(ResultType.TABLE);
+    }
+
+}
