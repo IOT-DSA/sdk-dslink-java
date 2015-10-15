91e37a50c60d5a4f7c3846a2b3880a7e8c3d46ad
samrg472 <samrg472@gmail.com>
2015-10-15 10:32:52 -0700

    Pretty encode the output of serialized data

--- sdk/dslink/src/main/java/org/dsa/iot/dslink/serializer/SerializationManager.java
+++ sdk/dslink/src/main/java/org/dsa/iot/dslink/serializer/SerializationManager.java
@@ -92,7 +92,7 @@ public class SerializationManager {
                 }
                 LOGGER.debug("Copying serialized data to a backup");
             }
-            String out = json.encode();
+            String out = json.encodePrettily();
             byte[] bytes = out.getBytes("UTF-8");
             FileUtils.write(file, bytes);
 
