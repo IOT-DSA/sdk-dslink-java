bf10b533b105ddf7690f11c0b68af16a4dbcc698
samrg472 <samrg472@gmail.com>
2015-10-20 17:42:15 -0700

    Support list stream caching

--- sdk/broker/src/main/java/org/dsa/iot/broker/stream/ListStream.java
+++ sdk/broker/src/main/java/org/dsa/iot/broker/stream/ListStream.java
@@ -9,15 +9,17 @@ import org.dsa.iot.dslink.util.json.JsonObject;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author Samuel Grenier
  */
 public class ListStream extends Stream {
 
-    // TODO: result caching
-
     private final Map<Client, Integer> reqMap = new ConcurrentHashMap<>();
+
+    private final ReentrantReadWriteLock cacheLock = new ReentrantReadWriteLock();
+    private final Map<String, JsonArray> cache = new ConcurrentHashMap<>();
 
     public ListStream(Responder responder, ParsedPath path) {
         super(responder, path);
@@ -26,6 +28,27 @@ public class ListStream extends Stream {
     @Override
     public void add(Client requester, int requesterRid) {
         reqMap.put(requester, requesterRid);
+        cacheLock.readLock().lock();
+        try {
+            JsonArray updates = new JsonArray();
+            for (JsonArray update : cache.values()) {
+                updates.add(update);
+            }
+
+            JsonObject resp = new JsonObject();
+            resp.put("rid", requesterRid);
+            resp.put("stream", StreamState.OPEN.getJsonName());
+            resp.put("updates", updates);
+
+            JsonArray resps = new JsonArray();
+            resps.add(resp);
+
+            JsonObject top = new JsonObject();
+            top.put("responses", resps);
+            requester.write(top.encode());
+        } finally {
+            cacheLock.readLock().unlock();
+        }
     }
 
     @Override
@@ -40,6 +63,32 @@ public class ListStream extends Stream {
 
     @Override
     public void dispatch(StreamState state, JsonObject response) {
+        JsonArray updates = response.get("updates");
+        if (updates != null) {
+            for (Object obj : updates) {
+                cacheLock.writeLock().lock();
+                try {
+                    if (obj instanceof JsonObject) {
+                        JsonObject json = (JsonObject) obj;
+                        String name = json.get("name");
+                        String change = json.get("change");
+                        if ("remove".equals(change)) {
+                            cache.remove(name);
+                        }
+                    } else if (obj instanceof JsonArray) {
+                        JsonArray array = (JsonArray) obj;
+                        String name = array.get(0);
+                        if (name.equals("$is")) {
+                            cache.clear();
+                        }
+                        cache.put(name, array);
+                    }
+                } finally {
+                    cacheLock.writeLock().unlock();
+                }
+            }
+        }
+
         JsonArray resps = new JsonArray();
         resps.add(response);
         JsonObject top = new JsonObject();
