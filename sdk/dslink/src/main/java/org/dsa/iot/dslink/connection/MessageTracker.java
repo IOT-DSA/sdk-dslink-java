eb05a227edb45cb2df4adae908efdffb994932a2
samrg472 <samrg472@gmail.com>
2015-11-19 11:39:02 -0800

    Add docs to the message tracker

--- sdk/dslink/src/main/java/org/dsa/iot/dslink/connection/MessageTracker.java
+++ sdk/dslink/src/main/java/org/dsa/iot/dslink/connection/MessageTracker.java
@@ -1,13 +1,34 @@
 package org.dsa.iot.dslink.connection;
 
 /**
+ * Keeps track of outgoing message IDs and incoming acks from and to the
+ * network.
+ *
  * @author Samuel Grenier
  */
 public interface MessageTracker {
 
+    /**
+     * An ack has been received from the network. This directly affects the
+     * missing ack count.
+     *
+     * @param ack Received ack ID.
+     */
     void ackReceived(int ack);
 
+    /**
+     * Retrieves the amount of missing acks. This can be used for network
+     * throttling if their is too many missing acks.
+     *
+     * @return Missing acks from the network.
+     */
     int missingAckCount();
 
+    /**
+     * Increments the message ID. This directly affects the missing ack
+     * count.
+     *
+     * @return Next message ID.
+     */
     int incrementMessageId();
 }
