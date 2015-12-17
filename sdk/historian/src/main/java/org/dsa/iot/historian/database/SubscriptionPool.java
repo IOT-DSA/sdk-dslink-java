5d0cbea16c46bbc7b64b3b76d568084a70109237
samrg472 <samrg472@gmail.com>
2015-12-17 12:58:52 -0800

    Revert "Ensure the current time is always used when the value arrives"
    This reverts commit f6b4f29c4bf4249b17ea00328f22fbbf194939fa.

--- sdk/historian/src/main/java/org/dsa/iot/historian/database/SubscriptionPool.java
+++ sdk/historian/src/main/java/org/dsa/iot/historian/database/SubscriptionPool.java
@@ -83,7 +83,6 @@ public class SubscriptionPool {
 
         @Override
         public void handle(SubscriptionValue event) {
-            event.getValue().setTime(System.currentTimeMillis());
             lock.readLock().lock();
             try {
                 for (Watch w : watches) {
