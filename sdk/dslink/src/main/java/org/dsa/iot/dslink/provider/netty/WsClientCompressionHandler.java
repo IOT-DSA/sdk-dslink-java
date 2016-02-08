872234f0800c54e6e3c25b6cb32dbc351f3ff29f
samrg472 <samrg472@gmail.com>
2016-02-08 10:54:38 -0800

    Clean up client compression handler

--- sdk/dslink/src/main/java/org/dsa/iot/dslink/provider/netty/WsClientCompressionHandler.java
+++ sdk/dslink/src/main/java/org/dsa/iot/dslink/provider/netty/WsClientCompressionHandler.java
@@ -1,41 +1,13 @@
-/*
- * Copyright 2014 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 package org.dsa.iot.dslink.provider.netty;
 
 import io.netty.handler.codec.http.websocketx.extensions.WebSocketClientExtensionHandler;
-import io.netty.handler.codec.http.websocketx.extensions.WebSocketClientExtensionHandshaker;
 import io.netty.handler.codec.http.websocketx.extensions.compression.DeflateFrameClientExtensionHandshaker;
 import io.netty.handler.codec.http.websocketx.extensions.compression.PerMessageDeflateClientExtensionHandshaker;
 
-import java.util.Arrays;
-
-/**
- * Extends <tt>io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientExtensionHandler</tt>
- * to handle the most common WebSocket Compression Extensions.
- *
- * See <tt>io.netty.example.http.websocketx.client.WebSocketClient</tt> for usage.
- */
 public class WsClientCompressionHandler extends WebSocketClientExtensionHandler {
 
-    /**
-     * Constructor with default configuration.
-     */
     public WsClientCompressionHandler() {
-        super(
-                new PerMessageDeflateClientExtensionHandshaker(6, true, 15, false, false),
+        super(new PerMessageDeflateClientExtensionHandshaker(6, true, 15, false, false),
                 new DeflateFrameClientExtensionHandshaker(false),
                 new DeflateFrameClientExtensionHandshaker(true));
     }
