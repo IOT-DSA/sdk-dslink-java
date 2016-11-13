9d1f07cbff87052267c44e25d1f5adae1ef20607
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-11-13 15:45:27 -0500

    Removed dependency on the PR branch of the testing link

--- ci/script/run_integrated_tests.sh
+++ ci/script/run_integrated_tests.sh
@@ -4,7 +4,7 @@ wget https://storage.googleapis.com/dart-archive/channels/stable/release/latest/
 unzip dartsdk-linux-x64-release.zip
 CURRENT_DIR="$(pwd)"
 
-git clone https://github.com/IOT-DSA/dslink-dart-test -b repacking-tested-links
+git clone https://github.com/IOT-DSA/dslink-dart-test
 
 cd dslink-dart-test/
 ${CURRENT_DIR}/dart-sdk/bin/pub get
