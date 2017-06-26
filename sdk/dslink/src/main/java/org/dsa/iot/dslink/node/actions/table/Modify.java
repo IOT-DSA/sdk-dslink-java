a6bb1d64ee04190cdf3c845d047cbb9e75ed3ba8
Daniel Shapiro <d.shapiro@berkeley.edu>
2017-06-26 11:59:09 -0700

    improve API

--- sdk/dslink/src/main/java/org/dsa/iot/dslink/node/actions/table/Modify.java
+++ sdk/dslink/src/main/java/org/dsa/iot/dslink/node/actions/table/Modify.java
@@ -1,65 +1,23 @@
 package org.dsa.iot.dslink.node.actions.table;
 
-public class Modify {
-	
-	private int removeFrom = -1;
-	private int insertBefore = -1;
-	
-	private Modify(int insertBefore) {
-		this.insertBefore = insertBefore;
-	}
-	
-	private Modify(int removeFrom, int insertBefore) {
-		this.removeFrom = removeFrom;
-		this.insertBefore = insertBefore;
-	}
-	
+public abstract class Modify {
 	
 	public boolean isReplace() {
-		return (removeFrom > -1 && insertBefore > -1);
-	}
+		return false;
+	};
 	
 	public boolean isInsert() {
-		return (removeFrom == -1 && insertBefore > -1);
-	}
-	
-	public int getInsertIndex() {
-		if (isInsert()) {
-			return insertBefore;
-		}
-		throw new UnsupportedOperationException("not an insert");
-	}
-	
-	public int getReplaceFrom() {
-		if (isReplace()) {
-			return removeFrom;
-		}
-		throw new UnsupportedOperationException("not a replace");
-	}
-	
-	public int getReplaceTo() {
-		if (isReplace()) {
-			return insertBefore;
-		}
-		throw new UnsupportedOperationException("not a replace");
-	}
-	
-	public String toString() {
-		if (isReplace()) {
-			return "replace " + Integer.toString(removeFrom) + "-" + Integer.toString(insertBefore);
-		} else {
-			return "insert " + Integer.toString(insertBefore);
-		}
-	}
+		return true;
+	};
 	
 	public static Modify fromString(String s) {
 		String[] arr = s.split("\\s+");
 		if (arr.length != 2) {
 			return null;
 		}
-		if (arr[0].equals("insert")) {
+		if ("insert".equals(arr[0])) {
 			return insert(Integer.parseInt(arr[1]));
-		} else if (arr[0].equals("replace")) {
+		} else if ("replace".equals(arr[0])) {
 			String[] arr2 = arr[1].split("-");
 			if (arr2.length != 2) {
 				return null;
@@ -71,11 +29,11 @@ public class Modify {
 	}
 	
 	public static Modify insert(int insertBefore) {
-		return new Modify(insertBefore);
+		return new Insert(insertBefore);
 	}
 	
 	public static Modify replace(int from, int to) {
-		return new Modify(from, to);
+		return new Replace(from, to);
 	}
 
 }
